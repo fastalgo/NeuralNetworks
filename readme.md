a4ff26f0fdeaf997e3151b2d3e6227548092693c
fastalgo <chinayouyang1991@gmail.com>
2016-05-25 12:01:18 -0700

    update readme.md and run.sh

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-Please download the data from http://www.cs.berkeley.edu/~youyang/data/ and then put them into the data/ folder
+The data will be downloaded from http://www.cs.berkeley.edu/~youyang/data/ and they will be put into the data/ folder. Please make sure your machine has the Internet access.
 
-Then just execute the following command:
+Then just execute the following command lines:
 
 $ chmod 777 run.sh
 
--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 rm execute;
+mkdir data
+wget http://www.cs.berkeley.edu/~youyang/data/mnist_data_train.txt
+wget http://www.cs.berkeley.edu/~youyang/data/mnist_label_train.txt
+wget http://www.cs.berkeley.edu/~youyang/data/mnist_data_test.txt
+wget http://www.cs.berkeley.edu/~youyang/data/mnist_label_test.txt
 g++ -o execute bp.c;
 ./execute 60000 10000 784 700 10 0.1 100 500 ./data/mnist_data_train.txt ./data/mnist_label_train.txt ./data/mnist_data_test.txt ./data/mnist_label_test.txt
