54dbda3004513c9ad078118a2c2b2cac96af5f3f
fastalgo <chinayouyang1991@gmail.com>
2016-05-25 12:08:03 -0700

    update readme.md and run.sh

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+This is a Naive implementation of 3-layer Artificial Neural Networks by the BP (Backpropagation) algorithm. The code is serial and is not dependent on any library.
+
 The data will be downloaded from http://www.cs.berkeley.edu/~youyang/data/ and they will be put into the data/ folder. Please make sure your machine has the Internet access.
 
 Then just execute the following command lines:
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 rm execute;
 mkdir data
+cd data
 wget http://www.cs.berkeley.edu/~youyang/data/mnist_data_train.txt
 wget http://www.cs.berkeley.edu/~youyang/data/mnist_label_train.txt
 wget http://www.cs.berkeley.edu/~youyang/data/mnist_data_test.txt
 wget http://www.cs.berkeley.edu/~youyang/data/mnist_label_test.txt
+cd ..
 g++ -o execute bp.c;
 ./execute 60000 10000 784 700 10 0.1 100 500 ./data/mnist_data_train.txt ./data/mnist_label_train.txt ./data/mnist_data_test.txt ./data/mnist_label_test.txt
